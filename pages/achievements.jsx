1406ac0017f1c63a15b999df7cb47c568b957bc6
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-06-26 13:48:14 -0500

    add svg thingamajiggy

--- pages/achievements.jsx
+++ pages/achievements.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import parseString from 'xml2js'
 
 /* notice: if you see this code please dont look cause im not stopping you but i want things to be fun */
@@ -78,7 +79,381 @@ export default function Achievements() {
       </Head>
       <div className="big-chonk">
         <div className="team-warning">TEAMING OR CHEATING FOR ACHIEVEMENTS IN ANY WAY IS BANNABLE (i can wipe all your data)</div>
-        <img src="assets/achievements.svg" />
+        <svg width="866pt" height="427pt"
+ viewBox="0.00 0.00 866.00 427.00" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<g id="graph0" class="graph" transform="scale(1 1) rotate(0) translate(4 423)">
+<polygon fill="white" stroke="transparent" points="-4,4 -4,-423 862,-423 862,4 -4,4"/>
+
+<g id="achievement&#45;hello" class="node">
+<title>a0</title>
+<polygon fill="none" stroke="lightgray" points="603,-419 548,-419 548,-364 603,-364 603,-419"/>
+<image xlink:href="assets/achievement-icons/hello.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="548" y="-419"/>
+</g>
+
+<g id="achievement&#45;luckyblock" class="node">
+<title>a1</title>
+<polygon fill="none" stroke="lightgray" points="128,-328 73,-328 73,-273 128,-273 128,-328"/>
+<image xlink:href="assets/achievement-icons/luckyblock.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="73" y="-328"/>
+</g>
+
+<g id="edge1" class="edge">
+<title>a0&#45;&gt;a1</title>
+<path fill="none" stroke="lightgray" d="M547.91,-400C446.53,-400 100.5,-400 100.5,-400 100.5,-400 100.5,-357.66 100.5,-328.22"/>
+</g>
+
+<g id="achievement&#45;caraccident" class="node">
+<title>a8</title>
+<polygon fill="none" stroke="lightgray" points="311,-328 256,-328 256,-273 311,-273 311,-328"/>
+<image xlink:href="assets/achievement-icons/caraccident.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="256" y="-328"/>
+</g>
+
+<g id="edge8" class="edge">
+<title>a0&#45;&gt;a8</title>
+<path fill="none" stroke="lightgray" d="M547.77,-382C474.88,-382 283.5,-382 283.5,-382 283.5,-382 283.5,-351.74 283.5,-328.09"/>
+</g>
+
+<g id="achievement&#45;murderer" class="node">
+<title>a17</title>
+<polygon fill="none" stroke="lightgray" points="603,-328 548,-328 548,-273 603,-273 603,-328"/>
+<image xlink:href="assets/achievement-icons/murderer.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="548" y="-328"/>
+</g>
+
+<g id="edge17" class="edge">
+<title>a0&#45;&gt;a17</title>
+<path fill="none" stroke="lightgray" d="M575.5,-363.7C575.5,-352.41 575.5,-339.4 575.5,-328.13"/>
+</g>
+
+<g id="achievement&#45;achiever" class="node">
+<title>a26</title>
+<polygon fill="none" stroke="lightgray" points="712,-328 657,-328 657,-273 712,-273 712,-328"/>
+<image xlink:href="assets/achievement-icons/achiever.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="657" y="-328"/>
+</g>
+
+<g id="edge26" class="edge">
+<title>a0&#45;&gt;a26</title>
+<path fill="none" stroke="lightgray" d="M603.01,-382C635.3,-382 684.5,-382 684.5,-382 684.5,-382 684.5,-351.74 684.5,-328.09"/>
+</g>
+
+<g id="achievement&#45;newhere" class="node">
+<title>a30</title>
+<polygon fill="none" stroke="lightgray" points="821,-328 766,-328 766,-273 821,-273 821,-328"/>
+<image xlink:href="assets/achievement-icons/newhere.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="766" y="-328"/>
+</g>
+
+<g id="edge30" class="edge">
+<title>a0&#45;&gt;a30</title>
+<path fill="none" stroke="lightgray" d="M603.11,-400C661.77,-400 793.5,-400 793.5,-400 793.5,-400 793.5,-357.66 793.5,-328.22"/>
+</g>
+
+<g id="achievement&#45;itssorare" class="node">
+<title>a2</title>
+<polygon fill="none" stroke="lightgray" points="55,-237 0,-237 0,-182 55,-182 55,-237"/>
+<image xlink:href="assets/achievement-icons/itssorare.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="0" y="-237"/>
+</g>
+
+<g id="edge2" class="edge">
+<title>a1&#45;&gt;a2</title>
+<path fill="none" stroke="lightgray" d="M72.9,-300C52.23,-300 27.5,-300 27.5,-300 27.5,-300 27.5,-263.76 27.5,-237.14"/>
+</g>
+
+<g id="achievement&#45;boxesandboxes" class="node">
+<title>a5</title>
+<polygon fill="none" stroke="lightgray" points="128,-237 73,-237 73,-182 128,-182 128,-237"/>
+<image xlink:href="assets/achievement-icons/boxesandboxes.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="73" y="-237"/>
+</g>
+
+<g id="edge5" class="edge">
+<title>a1&#45;&gt;a5</title>
+<path fill="none" stroke="lightgray" d="M100.5,-272.7C100.5,-261.41 100.5,-248.4 100.5,-237.13"/>
+</g>
+
+<g id="achievement&#45;legendary" class="node">
+<title>a3</title>
+<polygon fill="none" stroke="lightgray" points="55,-146 0,-146 0,-91 55,-91 55,-146"/>
+<image xlink:href="assets/achievement-icons/legendary.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="0" y="-146"/>
+</g>
+
+<g id="edge3" class="edge">
+<title>a2&#45;&gt;a3</title>
+<path fill="none" stroke="lightgray" d="M27.5,-181.7C27.5,-170.41 27.5,-157.4 27.5,-146.13"/>
+</g>
+
+<g id="achievement&#45;mythical" class="node">
+<title>a4</title>
+<polygon fill="none" stroke="lightgray" points="55,-55 0,-55 0,0 55,0 55,-55"/>
+<image xlink:href="assets/achievement-icons/mythical.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="0" y="-55"/>
+</g>
+
+<g id="edge4" class="edge">
+<title>a3&#45;&gt;a4</title>
+<path fill="none" stroke="lightgray" d="M27.5,-90.7C27.5,-79.41 27.5,-66.4 27.5,-55.13"/>
+</g>
+
+<g id="achievement&#45;professionallottery&#45;er" class="node">
+<title>a6</title>
+<polygon fill="none" stroke="lightgray" points="128,-146 73,-146 73,-91 128,-91 128,-146"/>
+<image xlink:href="assets/achievement-icons/professionallottery-er.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="73" y="-146"/>
+</g>
+
+<g id="edge6" class="edge">
+<title>a5&#45;&gt;a6</title>
+<path fill="none" stroke="lightgray" d="M100.5,-181.7C100.5,-170.41 100.5,-157.4 100.5,-146.13"/>
+</g>
+
+<g id="achievement&#45;ilikeboxes" class="node">
+<title>a7</title>
+<polygon fill="none" stroke="lightgray" points="128,-55 73,-55 73,0 128,0 128,-55"/>
+<image xlink:href="assets/achievement-icons/ilikeboxes.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="73" y="-55"/>
+</g>
+
+<g id="edge7" class="edge">
+<title>a6&#45;&gt;a7</title>
+<path fill="none" stroke="lightgray" d="M100.5,-90.7C100.5,-79.41 100.5,-66.4 100.5,-55.13"/>
+</g>
+
+<g id="achievement&#45;ohhiagain" class="node">
+<title>a9</title>
+<polygon fill="none" stroke="lightgray" points="201,-237 146,-237 146,-182 201,-182 201,-237"/>
+<image xlink:href="assets/achievement-icons/ohhiagain.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="146" y="-237"/>
+</g>
+
+<g id="edge9" class="edge">
+<title>a8&#45;&gt;a9</title>
+<path fill="none" stroke="lightgray" d="M255.74,-300C223.16,-300 173.5,-300 173.5,-300 173.5,-300 173.5,-263.76 173.5,-237.14"/>
+</g>
+
+<g id="achievement&#45;mechanicalabuse" class="node">
+<title>a11</title>
+<polygon fill="none" stroke="lightgray" points="274,-237 219,-237 219,-182 274,-182 274,-237"/>
+<image xlink:href="assets/achievement-icons/mechanicalabuse.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="219" y="-237"/>
+</g>
+
+<g id="edge11" class="edge">
+<title>a8&#45;&gt;a11</title>
+<path fill="none" stroke="lightgray" d="M265,-272.7C265,-261.41 265,-248.4 265,-237.13"/>
+</g>
+
+<g id="achievement&#45;spawndie" class="node">
+<title>a14</title>
+<polygon fill="none" stroke="lightgray" points="347,-237 292,-237 292,-182 347,-182 347,-237"/>
+<image xlink:href="assets/achievement-icons/spawndie.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="292" y="-237"/>
+</g>
+
+<g id="edge14" class="edge">
+<title>a8&#45;&gt;a14</title>
+<path fill="none" stroke="lightgray" d="M301.5,-272.7C301.5,-261.41 301.5,-248.4 301.5,-237.13"/>
+</g>
+
+<g id="achievement&#45;ikindahateyou" class="node">
+<title>a10</title>
+<polygon fill="none" stroke="lightgray" points="201,-146 146,-146 146,-91 201,-91 201,-146"/>
+<image xlink:href="assets/achievement-icons/ikindahateyou.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="146" y="-146"/>
+</g>
+
+<g id="edge10" class="edge">
+<title>a9&#45;&gt;a10</title>
+<path fill="none" stroke="lightgray" d="M173.5,-181.7C173.5,-170.41 173.5,-157.4 173.5,-146.13"/>
+</g>
+
+<g id="achievement&#45;verymechanicalabuse" class="node">
+<title>a12</title>
+<polygon fill="none" stroke="lightgray" points="274,-146 219,-146 219,-91 274,-91 274,-146"/>
+<image xlink:href="assets/achievement-icons/verymechanicalabuse.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="219" y="-146"/>
+</g>
+
+<g id="edge12" class="edge">
+<title>a11&#45;&gt;a12</title>
+<path fill="none" stroke="lightgray" d="M246.5,-181.7C246.5,-170.41 246.5,-157.4 246.5,-146.13"/>
+</g>
+
+<g id="achievement&#45;yourcarhatesyou" class="node">
+<title>a13</title>
+<polygon fill="none" stroke="lightgray" points="274,-55 219,-55 219,0 274,0 274,-55"/>
+<image xlink:href="assets/achievement-icons/yourcarhatesyou.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="219" y="-55"/>
+</g>
+
+<g id="edge13" class="edge">
+<title>a12&#45;&gt;a13</title>
+<path fill="none" stroke="lightgray" d="M246.5,-90.7C246.5,-79.41 246.5,-66.4 246.5,-55.13"/>
+</g>
+
+<g id="achievement&#45;karma" class="node">
+<title>a15</title>
+<polygon fill="none" stroke="lightgray" points="347,-146 292,-146 292,-91 347,-91 347,-146"/>
+<image xlink:href="assets/achievement-icons/karma.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="292" y="-146"/>
+</g>
+
+<g id="edge15" class="edge">
+<title>a14&#45;&gt;a15</title>
+<path fill="none" stroke="lightgray" d="M319.5,-181.7C319.5,-170.41 319.5,-157.4 319.5,-146.13"/>
+</g>
+
+<g id="achievement&#45;morekarma" class="node">
+<title>a16</title>
+<polygon fill="none" stroke="lightgray" points="420,-146 365,-146 365,-91 420,-91 420,-146"/>
+<image xlink:href="assets/achievement-icons/morekarma.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="365" y="-146"/>
+</g>
+
+<g id="edge16" class="edge">
+<title>a14&#45;&gt;a16</title>
+<path fill="none" stroke="lightgray" d="M347.1,-200C367.77,-200 392.5,-200 392.5,-200 392.5,-200 392.5,-169.74 392.5,-146.09"/>
+</g>
+
+<g id="achievement&#45;serialkiller" class="node">
+<title>a18</title>
+<polygon fill="none" stroke="lightgray" points="566,-237 511,-237 511,-182 566,-182 566,-237"/>
+<image xlink:href="assets/achievement-icons/serialkiller.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="511" y="-237"/>
+</g>
+
+<g id="edge18" class="edge">
+<title>a17&#45;&gt;a18</title>
+<path fill="none" stroke="lightgray" d="M557,-272.7C557,-261.41 557,-248.4 557,-237.13"/>
+</g>
+
+<g id="achievement&#45;speedykiller" class="node">
+<title>a23</title>
+<polygon fill="none" stroke="lightgray" points="639,-237 584,-237 584,-182 639,-182 639,-237"/>
+<image xlink:href="assets/achievement-icons/speedykiller.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="584" y="-237"/>
+</g>
+
+<g id="edge23" class="edge">
+<title>a17&#45;&gt;a23</title>
+<path fill="none" stroke="lightgray" d="M593.5,-272.7C593.5,-261.41 593.5,-248.4 593.5,-237.13"/>
+</g>
+
+<g id="achievement&#45;psychokiller" class="node">
+<title>a19</title>
+<polygon fill="none" stroke="lightgray" points="493,-146 438,-146 438,-91 493,-91 493,-146"/>
+<image xlink:href="assets/achievement-icons/psychokiller.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="438" y="-146"/>
+</g>
+
+<g id="edge19" class="edge">
+<title>a18&#45;&gt;a19</title>
+<path fill="none" stroke="lightgray" d="M510.9,-218C490.23,-218 465.5,-218 465.5,-218 465.5,-218 465.5,-175.66 465.5,-146.22"/>
+</g>
+
+<g id="achievement&#45;cerealkiller" class="node">
+<title>a21</title>
+<polygon fill="none" stroke="lightgray" points="566,-146 511,-146 511,-91 566,-91 566,-146"/>
+<image xlink:href="assets/achievement-icons/cerealkiller.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="511" y="-146"/>
+</g>
+
+<g id="edge21" class="edge">
+<title>a18&#45;&gt;a21</title>
+<path fill="none" stroke="lightgray" d="M529.33,-181.7C529.33,-170.41 529.33,-157.4 529.33,-146.13"/>
+</g>
+
+<g id="achievement&#45;spawnkill" class="node">
+<title>a22</title>
+<polygon fill="none" stroke="lightgray" points="639,-146 584,-146 584,-91 639,-91 639,-146"/>
+<image xlink:href="assets/achievement-icons/spawnkill.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="584" y="-146"/>
+</g>
+
+<g id="edge22" class="edge">
+<title>a18&#45;&gt;a22</title>
+<path fill="none" stroke="lightgray" d="M547.67,-181.94C547.67,-169.7 547.67,-158 547.67,-158 547.67,-158 602.33,-158 602.33,-158 602.33,-158 602.33,-152.9 602.33,-146.36"/>
+</g>
+
+<g id="achievement&#45;overpowered" class="node">
+<title>a20</title>
+<polygon fill="none" stroke="lightgray" points="493,-55 438,-55 438,0 493,0 493,-55"/>
+<image xlink:href="assets/achievement-icons/overpowered.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="438" y="-55"/>
+</g>
+
+<g id="edge20" class="edge">
+<title>a19&#45;&gt;a20</title>
+<path fill="none" stroke="lightgray" d="M465.5,-90.7C465.5,-79.41 465.5,-66.4 465.5,-55.13"/>
+</g>
+
+<g id="achievement&#45;sanicthehedgehog" class="node">
+<title>a24</title>
+<polygon fill="none" stroke="lightgray" points="712,-146 657,-146 657,-91 712,-91 712,-146"/>
+<image xlink:href="assets/achievement-icons/sanicthehedgehog.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="657" y="-146"/>
+</g>
+
+<g id="edge24" class="edge">
+<title>a23&#45;&gt;a24</title>
+<path fill="none" stroke="lightgray" d="M620.67,-181.64C620.67,-175.1 620.67,-170 620.67,-170 620.67,-170 684.5,-170 684.5,-170 684.5,-170 684.5,-158.3 684.5,-146.06"/>
+</g>
+
+<g id="achievement&#45;yourescary" class="node">
+<title>a25</title>
+<polygon fill="none" stroke="lightgray" points="712,-55 657,-55 657,0 712,0 712,-55"/>
+<image xlink:href="assets/achievement-icons/yourescary.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="657" y="-55"/>
+</g>
+
+<g id="edge25" class="edge">
+<title>a24&#45;&gt;a25</title>
+<path fill="none" stroke="lightgray" d="M684.5,-90.7C684.5,-79.41 684.5,-66.4 684.5,-55.13"/>
+</g>
+
+<g id="achievement&#45;overachiever" class="node">
+<title>a27</title>
+<polygon fill="none" stroke="lightgray" points="712,-237 657,-237 657,-182 712,-182 712,-237"/>
+<image xlink:href="assets/achievement-icons/overachiever.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="657" y="-237"/>
+</g>
+
+<g id="edge27" class="edge">
+<title>a26&#45;&gt;a27</title>
+<path fill="none" stroke="lightgray" d="M684.5,-272.7C684.5,-261.41 684.5,-248.4 684.5,-237.13"/>
+</g>
+
+<g id="achievement&#45;chonkachiever" class="node">
+<title>a29</title>
+<polygon fill="none" stroke="lightgray" points="785,-237 730,-237 730,-182 785,-182 785,-237"/>
+<image xlink:href="assets/achievement-icons/chonkachiever.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="730" y="-237"/>
+</g>
+
+<g id="edge29" class="edge">
+<title>a26&#45;&gt;a29</title>
+<path fill="none" stroke="lightgray" d="M712.17,-300C729.28,-300 748,-300 748,-300 748,-300 748,-263.76 748,-237.14"/>
+</g>
+
+<g id="achievement&#45;toogood" class="node">
+<title>a28</title>
+<polygon fill="none" stroke="lightgray" points="785,-146 730,-146 730,-91 785,-91 785,-146"/>
+<image xlink:href="assets/achievement-icons/toogood.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="730" y="-146"/>
+</g>
+
+<g id="edge28" class="edge">
+<title>a27&#45;&gt;a28</title>
+<path fill="none" stroke="lightgray" d="M712.16,-209C717.26,-209 721,-209 721,-209 721,-209 721,-118 721,-118 721,-118 724.74,-118 729.84,-118"/>
+</g>
+
+<g id="achievement&#45;veteran" class="node">
+<title>a31</title>
+<polygon fill="none" stroke="lightgray" points="858,-237 803,-237 803,-182 858,-182 858,-237"/>
+<image xlink:href="assets/achievement-icons/veteran.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="803" y="-237"/>
+</g>
+
+<g id="edge31" class="edge">
+<title>a30&#45;&gt;a31</title>
+<path fill="none" stroke="lightgray" d="M812,-272.7C812,-261.41 812,-248.4 812,-237.13"/>
+</g>
+
+<g id="achievement&#45;og" class="node">
+<title>a32</title>
+<polygon fill="none" stroke="lightgray" points="858,-146 803,-146 803,-91 858,-91 858,-146"/>
+<image xlink:href="assets/achievement-icons/og.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="803" y="-146"/>
+</g>
+
+<g id="edge32" class="edge">
+<title>a31&#45;&gt;a32</title>
+<path fill="none" stroke="lightgray" d="M830.5,-181.7C830.5,-170.41 830.5,-157.4 830.5,-146.13"/>
+</g>
+
+<g id="achievement&#45;ancient" class="node">
+<title>a33</title>
+<polygon fill="none" stroke="lightgray" points="858,-55 803,-55 803,0 858,0 858,-55"/>
+<image xlink:href="assets/achievement-icons/ancient.jpg" width="55px" height="55px" preserveAspectRatio="xMinYMin meet" x="803" y="-55"/>
+</g>
+
+<g id="edge33" class="edge">
+<title>a32&#45;&gt;a33</title>
+<path fill="none" stroke="lightgray" d="M830.5,-90.7C830.5,-79.41 830.5,-66.4 830.5,-55.13"/>
+</g>
+</g>
+</svg>
       </div>
     </div>
   )
