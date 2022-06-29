83c6f69526d1f290ecf91ab32eb1d10d0e2aa6ae
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-06-28 19:39:49 -0500

    Create achievements.js

---
+++ assets/scripts/achievements.js
@@ -0,0 +1,10 @@
+const wrapper = document.querySelector(".achievement-wrapper");
+fetch("./assets/achievements.svg")
+  .then(e => e.text())
+  .then(e => {
+    var file = e;
+
+    // do stuff
+  
+    wrapper.innerHTML = file;
+  });
